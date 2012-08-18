d60d91963ef72d1f6657a53c7e8b48b39709c315
augustusbr <augusto_dz@yahoo.com.br>
2012-08-18 16:26:03 -0300

    initial

---
+++ BigBrother/src/com/augustusbr/bigbrother/BigBrother.java
@@ -0,0 +1,12 @@
+package com.augustusbr.bigbrother;
+
+public final class BigBrother {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("LOL");
+	}
+
+}
